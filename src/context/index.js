cb65d6b27f2ee6ea99820ec7c67ee28dea919042
Nike98 <nikhilagarwal20@yahoo.com>
2021-07-30 16:54:21 +0530

    combined all context refs in one file

---
+++ src/context/index.js
@@ -0,0 +1,20 @@
+import {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjectsValue,
+} from "./prjects-context";
+
+import {
+  SelectedProjectContext,
+  SelectedProjectsProvider,
+  useSelectedProjectValue,
+} from "./selected-project-context";
+
+export {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjectsValue,
+  SelectedProjectContext,
+  SelectedProjectsProvider,
+  useSelectedProjectValue,
+};
