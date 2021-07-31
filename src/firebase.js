392db4e91b9ed4265568892dd1225880f699749f
Nike98 <nikhilagarwal20@yahoo.com>
2021-07-31 21:09:29 +0530

    added firebase info file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyBHW5PJC1g5EqPx3838MJf7rtsPHs301Tc",
+  authDomain: "todo-711db.firebaseapp.com",
+  databaseURL: "https://todo-711db-default-rtdb.firebaseio.com",
+  projectId: "todo-711db",
+  storageBucket: "todo-711db.appspot.com",
+  messagingSenderId: "942494387834",
+  appId: "1:942494387834:web:d6433415b0d2eef41775c3",
+});
+
+export { firebaseConfig as firebase };
